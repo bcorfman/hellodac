ec369e29ea5a81b77859b63db8ba7033c04209ec
Brandon Corfman <brandon.c.corfman.civ@mail.mil>
2021-08-05 10:42:39 -0400

    FEATURE: testable hello_dac function

---
+++ hello.py
@@ -0,0 +1,11 @@
+def hello_dac():
+    # this is Brandon's code for Hello World
+    return 'Hello DAC!'
+
+
+def main():
+    print(hello_dac())
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import hello_dac
+
+
+def test_hello_dac():
+    assert hello_dac() == 'Hello DAC!'
